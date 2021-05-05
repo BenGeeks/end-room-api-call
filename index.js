c980faeff389dc29fd26a1b0c053b93efbb54789
Ben Gican <b3nhur77@gmail.com>
2021-05-05 08:03:36 +0800

    updated!

--- index.js
+++ index.js
@@ -3,36 +3,32 @@ import nodemailer from 'nodemailer';
 
 const app = express();
 
-const logRequestStart = (req, res, next) => {
-    const dataNeeded = (`${req.method} ${req.originalUrl}`);
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'b3nmailer2021@gmail.com',
-          pass: '"+f;#dv4d)D-#AH!QgE-fqK.!]Q>-?AGZxhvsC%mc~[6rqxWwS'
-        }
-    });
-    
-    const mailOptions = {
-        from: 'b3nmailer2021@gmail.com',
-        to: 'b3nhur77@gmail.com, jan@flowapp.com, tharinda@flowapp.com, saroj@flowapp.com', 
-        subject: dataNeeded,
-        text: `Here is the data that you need ${dataNeeded}`
-    };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-    });
-    next();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', function (req, res, next) {
+  const meetingNo = req.query.meetingNo;
+  res.send(`<h2>The meeting number is: ${meetingNo}</h2>`);
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'b3nmailer2021@gmail.com',
+      pass: '"+f;#dv4d)D-#AH!QgE-fqK.!]Q>-?AGZxhvsC%mc~[6rqxWwS',
+    },
+  });
+
+  const mailOptions = {
+    from: 'b3nmailer2021@gmail.com',
+    to: 'b3nhur77@gmail.com, jan@flowapp.com, tharinda@flowapp.com, saroj@flowapp.com',
+    subject: meetingNo,
+    text: `The meeting number is ${meetingNo}.`,
   };
-  
-  const PORT = process.env.PORT || 5000;
-  app.use(logRequestStart);
-  app.get('/', function(req, res, next) {
-    res.send('<h1>Welcome to my world!</h1>');
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 });
-  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
